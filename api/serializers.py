f580c1b5b2e7bedfd68c9f8af2d82597f4e440d0
mohammad <mohammadpagard2003@gmail.com>
2022-06-22 21:59:53 +0430

    Set default permission in settings.py & شعفauthor.username instead of author.id...

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    def get_author(self, obj):
+        return [
+            obj.author.username,
+            obj.author.first_name,
+            obj.author.last_name,
+        ]
+
+    author = serializers.SerializerMethodField("get_author")
+
     class Meta:
         model = Article
         exclude = ['created', 'updated']
